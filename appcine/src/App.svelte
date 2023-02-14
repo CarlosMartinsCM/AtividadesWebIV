9062c5ab8bce1f0caa4c43f0e9cf2e6f3f844854
Carlos Martins <carlosmartins.ifpr2020@gmail.com>
2023-02-13 22:18:34 -0300

    update_CRUD

--- appcine/src/App.svelte
+++ appcine/src/App.svelte
@@ -2,6 +2,7 @@
   import Movie from './lib/Movie.svelte'
   import User from './lib/User.svelte'
   import Person from './lib/Person.svelte'
+  import UserList from './lib/UserList.svelte';
   let menu = 1;
 </script>
 
@@ -9,14 +10,17 @@
     <p>Projeto Web IV</p>
     
     <ul class="nav">
-        <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Users</a></li>
-        <li><a href="/" on:click|preventDefault={() => (menu = 2)}>Movies</a></li>
-        <li><a href="/" on:click|preventDefault={() => (menu = 3)}>Artista</a></li>
+        <li class="botao"><a href="/" on:click|preventDefault={() => (menu = 1)}>Cadastrar Usuário</a></li>
+        <li class="botao"><a href="/" on:click|preventDefault={() => (menu = 11)}>Listar Users</a></li>
+        <li class="botao"><a href="/" on:click|preventDefault={() => (menu = 2)}>Movies</a></li>
+        <li class="botao"><a href="/" on:click|preventDefault={() => (menu = 3)}>Artista</a></li>
     </ul>
 
     <div class="card">
     {#if menu === 1}
         <User/>
+    {:else if menu === 11}
+        <UserList/>
     {:else if menu === 2}
         <Movie/>
     {:else if menu === 3}
@@ -33,9 +37,14 @@ ul.nav {
     display: flex;
     padding: 0;
     place-content: center;
+    
 }
 ul.nav li{
     display: inline-block;
     padding: 0px 5px;
+}
+.botao {
+    border: 1px solid black;
+    padding: 10;
 }
 </style>
--- appcine/src/lib/User.svelte
+++ appcine/src/lib/User.svelte
@@ -1,42 +1,65 @@
 <script>
-let resposta = "";
-async function sendForm(e){
-    // envia o formulario no formato json
-    let formData = new FormData(e.target);
-    let data = Object.fromEntries(formData.entries());
-    const res = await fetch('http://localhost:8000/user/create',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-    const json = await res.json();
-    resposta = JSON.stringify(json);
-}
+    import UserList from "./UserList.svelte";
+
+    let resposta = "";
+    let cadastrou = false;
+    async function sendForm(e) {
+        // envia o formulario no formato json
+        let formData = new FormData(e.target);
+        let data = Object.fromEntries(formData.entries());
+        cadastrou = true;
+        const res = await fetch("http://localhost:8000/user/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        const json = await res.json();
+        resposta = JSON.stringify(json);
+    }
 </script>
 
+{#if cadastrou === false}
+    <h2>New user</h2>
+    <form class="crud" on:submit|preventDefault={sendForm}>
+        <input
+            type="text"
+            name="name"
+            placeholder="User name"
+            required
+            autocomplete="off"
+        />
+        <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            required
+            autocomplete="off"
+        />
+        <input
+            type="text"
+            name="password"
+            placeholder="password"
+            required
+            autocomplete="off"
+        />
+        <input type="submit" value="add" />
+    </form>
+{/if}
 
-
-<h2>New user</h2>
-
-<p>{resposta}</p>
-
-<form class="crud" on:submit|preventDefault={sendForm}>
-    <input type="text" name="name" placeholder="User name" required autocomplete="off">
-    <input type="text" name="email" placeholder="Email" required autocomplete="off">
-    <input type="text" name="password" placeholder="password" required autocomplete="off">
-    <input type="submit" value="add">
-</form>
+{#if cadastrou === true}
+    <svelte:component this={UserList} />
+{/if}
 
 <style>
-form.crud{
-    display: grid;
-    grid-template-columns: 1fr;
-    gap: 5px;
-    row-gap: 10px;
-}
-.crud input[type=submit]{
-    justify-self: baseline;
-}
+    form.crud {
+        display: grid;
+        grid-template-columns: 1fr;
+        gap: 5px;
+        row-gap: 10px;
+    }
+    .crud input[type="submit"] {
+        justify-self: baseline;
+    }
 </style>
